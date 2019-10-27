5cbc5a3d866b22ca5e7984e1d3c5d2b4bb95174a
ArmenianDavid <imast.un@mail.ru>
2019-10-27 17:35:38 +0400

    Updates

---
+++ precache-manifest.7f29d1a4226801e89b32380e5ba95719.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513b99d06a07c5e4bb8d3a5e28cf402e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "59a4eb0da3d1f45d8c47",
+    "url": "/Portfolio/static/css/main.13b57138.chunk.css"
+  },
+  {
+    "revision": "101ada89f1ab1d6fd541",
+    "url": "/Portfolio/static/js/2.598d69a0.chunk.js"
+  },
+  {
+    "revision": "59a4eb0da3d1f45d8c47",
+    "url": "/Portfolio/static/js/main.102d9564.chunk.js"
+  },
+  {
+    "revision": "63fe68486ecb758e5a56",
+    "url": "/Portfolio/static/js/runtime-main.b0957d76.js"
+  }
+]);
